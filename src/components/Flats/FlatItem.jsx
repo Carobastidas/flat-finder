b0f3a47404d4fd29c2f698a7a9a06b5ef74910cf
wladimov <info@wladimov.com>
2024-08-10 20:16:47 -0500

    [REPLACE] element <a> to <Link> in component FlatItem

--- src/components/Flats/FlatItem.jsx
+++ src/components/Flats/FlatItem.jsx
@@ -1,15 +1,17 @@
 //Componente para mostrar informacion individual de un flat
 
+import { Link } from "react-router-dom";
+
 function FlatItem () {
     return (
         <article className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <a href="/">
+          <picture>
             <img
               className="rounded-t-lg"
               src="https://images.pexels.com/photos/14465265/pexels-photo-14465265.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               alt=""
             />
-          </a>
+          </picture>
           <div className="p-5">
             <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
             Quito, Las Vegas, N-767
@@ -21,7 +23,7 @@ function FlatItem () {
                 $100
             </p>
             <div className="flex justify-between">
-              <a
+              <Link
                 href="/"
                 className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
@@ -41,7 +43,7 @@ function FlatItem () {
                     d="M1 5h12m0 0L9 1m4 4L9 9"
                   />
                 </svg>
-              </a>
+              </Link>
 
               <div className="flex gap-4">
                 <svg
