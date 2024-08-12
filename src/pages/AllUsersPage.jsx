c313b3be99e6ea4f457762120179c90c58fd303e
wladimov <info@wladimov.com>
2024-08-12 13:18:24 -0500

    [ADD] component AllUsersPage

--- src/pages/AllUsersPage.jsx
+++ src/pages/AllUsersPage.jsx
@@ -1,1 +1,14 @@
 //Pagina para mostrar todo los usuarios registrados (solo la puede ver el admin)
+
+import { Navbar } from "../components/Commons/Navbar";
+
+function ProfilePage () {
+    return (
+        <>
+            <Navbar />
+            <h1>Profile Page</h1>
+        </>
+    );
+}
+
+export { ProfilePage };
