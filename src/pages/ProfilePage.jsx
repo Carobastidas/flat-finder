4de2e2f7d6832f9aea94a9b6d577db6dad54e13d
wladimov <info@wladimov.com>
2024-08-12 13:15:25 -0500

    [ADD] component ProfilePage

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,1 +1,14 @@
 //Pagina para ver el perfil del usuario
+
+import { Navbar } from "../components/Commons/Navbar";
+
+function ProfilePage () {
+    return(
+        <>
+            <Navbar />
+            <h1>Profile Page</h1>
+        </>
+    );
+}
+
+export { ProfilePage }
