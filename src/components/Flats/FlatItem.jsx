f0b01462a957bcd9ccad554a9433a48cbc6f2a0b
wladimov <info@wladimov.com>
2024-08-14 12:06:28 -0500

    Add link to view flat

--- src/components/Flats/FlatItem.jsx
+++ src/components/Flats/FlatItem.jsx
@@ -24,7 +24,7 @@ function FlatItem () {
             </p>
             <div className="flex justify-between">
               <Link
-                href="/"
+                to="/flat-details"
                 className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 View flat
