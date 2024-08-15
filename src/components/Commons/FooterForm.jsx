867be11952a2c067fb4d5f1f9f7115230932fe3d
wladimov <info@wladimov.com>
2024-08-14 23:44:06 -0500

    Refactor component FooterForm

--- src/components/Commons/FooterForm.jsx
+++ src/components/Commons/FooterForm.jsx
@@ -1,16 +1,18 @@
-function FooterFrom({ message, linkText }) {
-    return (
-        <p className="text-center text-sm text-gray-400">
-              {message} &nbsp;
-              <a
-                href="/register"
-                className="font-semibold text-indigo-500 focus:text-indigo-600 focus:underline focus:outline-none"
-              >
-                {linkText}
-              </a>
-              .
-        </p>
-    );
+import { Link } from "react-router-dom";
+
+function FooterFrom({ message, linkText, to }) {
+  return (
+    <p className="text-center text-sm text-gray-400">
+      {message} &nbsp;
+      <Link
+        to={to}
+        className="font-semibold text-indigo-500 focus:text-indigo-600 focus:underline focus:outline-none"
+      >
+        {linkText}
+      </Link>
+      .
+    </p>
+  );
 }
 
 export { FooterFrom };
