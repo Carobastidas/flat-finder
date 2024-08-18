1969b6ca53b4098deb799f7ac8e61ef49cac53a1
wladimov <info@wladimov.com>
2024-08-17 23:22:35 -0500

    Add prop type

--- src/components/Commons/ButtonPrimaryForm.jsx
+++ src/components/Commons/ButtonPrimaryForm.jsx
@@ -1,8 +1,9 @@
-function ButtonPrimaryForm({ text }) {
+function ButtonPrimaryForm({ text, type, onClick }) {
   return (
     <div className="mb-6">
       <button
-        type="button"
+        type={type}
+        onClick={onClick}
         className="w-full rounded bg-indigo-500 pt-2 pb-3 text-white duration-100 ease-in-out hover:bg-indigo-600 focus:outline-none"
       >
         {text}
