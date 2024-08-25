edc8a478ac7222c90bb6c77b307395da635eec4e
wladimov <info@wladimov.com>
2024-08-24 22:36:09 -0500

    Refactor

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,50 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/authContext";
 import { LoginForm } from "../components/Users/LoginForm";
+import {
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithRedirect,
+} from "firebase/auth";
+import { auth } from "../config/firebase";
 
 function LoginPage() {
+  const navigate = useNavigate();
+  const { currentUser } = useAuth();
+
+  useEffect(() => {
+    if (currentUser) {
+      navigate("/home");
+    }
+  }, [currentUser, navigate]);
+
+  const handleLogin = async (email, password) => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/home");
+    } catch (error) {
+      console.error("Error logging in:", error);
+      // Aquí puedes mostrar un mensaje de error al usuario
+    }
+  };
+
+  const handleLoginWithGoogle = async () => {
+    const googleProvider = new GoogleAuthProvider();
+    try {
+      await signInWithRedirect(auth, googleProvider);
+      navigate("/home");
+    } catch (error) {
+      console.error("Error logging in with Google:", error);
+      // Aquí puedes mostrar un mensaje de error al usuario
+    }
+  };
+
   return (
-    <LoginForm />
+    <LoginForm
+      onLogin={handleLogin}
+      onLoginWithGoogle={handleLoginWithGoogle}
+    />
   );
 }
 
