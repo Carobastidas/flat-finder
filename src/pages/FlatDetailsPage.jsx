ce9160b595a05b61b46f28ca4bde1a84d03c1719
wladimov <info@wladimov.com>
2024-08-28 17:52:25 -0500

    Add FlatDetailsPage

--- src/pages/FlatDetailsPage.jsx
+++ src/pages/FlatDetailsPage.jsx
@@ -1,9 +1,66 @@
-//Pagina para ver la informacion detallada de un flat
-
+import { useState, useEffect } from "react";
+import { Navbar } from "../components/Commons/Navbar";
 import { FlatView } from "../components/Flats/FlatView";
+import { MessageForm } from "../components/Messages/MessageForm";
+import { MessageList } from "../components/Messages/MessageList";
+import { useParams } from "react-router-dom";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../config/firebase";
+import { useAuth } from "../context/authContext";
 
 function FlatDetailsPage() {
-  return <FlatView />;
+  const { id } = useParams();
+  const { currentUser } = useAuth();
+  const [isOwner, setIsOwner] = useState(false);
+  const [flat, setFlat] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchFlatData = async () => {
+      try {
+        const flatRef = doc(db, "flats", id);
+        const flatSnap = await getDoc(flatRef);
+        if (flatSnap.exists()) {
+          const flatData = { id: flatSnap.id, ...flatSnap.data() };
+          setFlat(flatData);
+          setIsOwner(flatData.ownerEmail === currentUser.email);
+        } else {
+          setError("No se encontró el flat.");
+        }
+      } catch (error) {
+        setError("Error al obtener los detalles del flat");
+        console.error("Error al obtener los detalles del flat:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchFlatData();
+  }, [id, currentUser]);
+
+  if (!currentUser) {
+    return <div>Cargando...</div>; // Mostrar algún mensaje o redirigir
+  }
+
+  return (
+    <>
+      <Navbar />
+      <section className="flex flex-col items-center justify-center gap-4 mt-28 mr-4 ml-4 mb-16">
+        {loading ? (
+          <p>Cargando...</p>
+        ) : error ? (
+          <p>{error}</p>
+        ) : (
+          flat && <FlatView flat={flat} isOwner={isOwner} />
+        )}
+        <p>uid del usuario: {currentUser.uid}</p>
+        <p>Nombre del usuario logueado:{currentUser.displayName}</p>
+        <MessageList />
+        <MessageForm />
+      </section>
+    </>
+  );
 }
 
 export { FlatDetailsPage };
