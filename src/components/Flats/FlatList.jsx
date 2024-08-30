7be9d9535471609d7a6db416fc927a59cae864a9
wladimov <info@wladimov.com>
2024-08-30 16:30:48 -0500

    Fetch flats by owner ID in MyFlatsPage

--- src/components/Flats/FlatList.jsx
+++ src/components/Flats/FlatList.jsx
@@ -1,56 +1,12 @@
 // Componente para listar los flats
-
-import { useState, useEffect } from "react";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../../config/firebase.js";
 import { FlatItem } from "../Flats/FlatItem.jsx";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
 
-function FlatList() {
-  const [flats, setFlats] = useState([]);
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const fetchFlats = async () => {
-      try {
-        const flatsCollection = collection(db, "flats");
-        const flatsSnapshot = await getDocs(flatsCollection);
-        const flatsList = flatsSnapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-          url: doc.data().imageUrl,
-        }));
-
-        setFlats(flatsList);
-      } catch (error) {
-        console.error("Error al obtener los pisos:", error);
-      }
-    };
-
-    fetchFlats();
-  }, []);
-
-  useEffect(() => {
-    const auth = getAuth();
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-    });
-
-    return () => unsubscribe();
-  }, []);
-
+function FlatList({ flats }) {
   return (
     <>
-      <section className="flex flex-wrap justify-center gap-4 mt-24 mr-4 ml-4 mb-16">
-        {user && (
-          <div className="w-full text-center mb-8">
-            <h1>Bienvenido, {user.displayName} </h1>
-          </div>
-        )}
-        {flats.map((flat) => (
-          <FlatItem key={flat.id} {...flat} />
-        ))}
-      </section>
+      {flats.map((flat) => (
+        <FlatItem key={flat.id} {...flat} />
+      ))}
     </>
   );
 }
