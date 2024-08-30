90959d75a4d5f5061e259a4f73e96f8d7406f0af
wladimov <info@wladimov.com>
2024-08-30 12:26:23 -0500

    Add useCallback and fetch flat ID in messages processing

--- src/pages/FlatDetailsPage.jsx
+++ src/pages/FlatDetailsPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { Navbar } from "../components/Commons/Navbar";
 import { FlatView } from "../components/Flats/FlatView";
 import { MessageForm } from "../components/Messages/MessageForm";
@@ -9,22 +9,40 @@ import { db } from "../config/firebase";
 import { useAuth } from "../context/authContext";
 
 function FlatDetailsPage() {
-  const { id } = useParams(); // id del flat
-  const { currentUser } = useAuth();
-  const [isOwner, setIsOwner] = useState(false);
+  const { id } = useParams();
+  const { currentUser, loadMessages } = useAuth();
   const [flat, setFlat] = useState(null);
+  const [isOwner, setIsOwner] = useState(false);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [flatId, setFlatId] = useState(null);
+  const memoizedLoadMessages = useCallback(
+    (flatId) => {
+      if (flatId) {
+        loadMessages(flatId);
+      }
+    },
+    [loadMessages]
+  );
 
   useEffect(() => {
     const fetchFlatData = async () => {
       try {
         const flatRef = doc(db, "flats", id);
         const flatSnap = await getDoc(flatRef);
+
         if (flatSnap.exists()) {
           const flatData = { id: flatSnap.id, ...flatSnap.data() };
           setFlat(flatData);
           setIsOwner(flatData.ownerEmail === currentUser.email);
+
+          // Solo actualizar flatId si ha cambiado
+          if (flatId !== flatSnap.id) {
+            setFlatId(flatSnap.id);
+          }
+
+          // Cargar mensajes solo si flatId está disponible
+          memoizedLoadMessages(flatSnap.id);
         } else {
           setError("No se encontró el flat.");
         }
@@ -36,8 +54,10 @@ function FlatDetailsPage() {
       }
     };
 
-    fetchFlatData();
-  }, [id, currentUser]);
+    if (id && currentUser) {
+      fetchFlatData();
+    }
+  }, [id, currentUser, flatId, memoizedLoadMessages]);
 
   if (!currentUser) {
     return <div>Cargando...</div>;
@@ -56,8 +76,8 @@ function FlatDetailsPage() {
         )}
         <p>uid del usuario: {currentUser.uid}</p>
         <p>Nombre del usuario logueado: {currentUser.displayName}</p>
-        <MessageList />
-        <MessageForm flatId={id} /> {/* Pasa el flatId aquí */}
+        {flat && <MessageList flatId={flat.id} flat={flat} />}
+        <MessageForm flatId={flat?.id} />
       </section>
     </>
   );
