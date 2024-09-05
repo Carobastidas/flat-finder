6f11e94edcc1127a3bd693b4cdafceb5d005af6e
wladimov <info@wladimov.com>
2024-09-05 18:41:24 -0500

    Add icons & features

--- src/components/Flats/FlatList.jsx
+++ src/components/Flats/FlatList.jsx
@@ -1,11 +1,19 @@
 // Componente para listar los flats
 import { FlatItem } from "../Flats/FlatItem.jsx";
 
-function FlatList({ flats }) {
+function FlatList({ flats, onDeleteFlat }) {
   return (
     <>
       {flats.map((flat) => (
-        <FlatItem key={flat.id} {...flat} />
+        <FlatItem
+          key={flat.id}
+          {...flat}
+          displayFavoriteIcon="hidden"
+          displayPencilIcon="block"
+          displayTrashIcon="block"
+          displayHomeIcon="block"
+          onDelete={onDeleteFlat}
+        />
       ))}
     </>
   );
